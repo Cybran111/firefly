0786e87ee9e8ad0bf32c86a53f07ea01714eedfc
n.lyubchich <cybran111@gmail.com>
2015-07-03 16:17:45 +0300

    Cache backend

--- example_middleware.py
+++ example_middleware.py
@@ -5,5 +5,19 @@ def reverse_middleware(app):
         response = app(environ, start_response)
         return [s[::-1] for s in response]
     return wrapped_app
+
+CACHE_DICT = dict()
+
+def cache_middleware(app):
+    def wrapper(environ, start_response):
+        if environ['PATH_INFO'] in CACHE_DICT.keys():
+            return CACHE_DICT[environ['PATH_INFO']]
+        else:
+            response = app(environ, start_response)
+            CACHE_DICT[environ['PATH_INFO']] = response
+            return response
+
+    return wrapper
+
 if __name__ == '__main__':
-    make_server('', 8000, reverse_middleware(simple_app)).serve_forever()
+    make_server('', 8000, cache_middleware(simple_app)).serve_forever()
